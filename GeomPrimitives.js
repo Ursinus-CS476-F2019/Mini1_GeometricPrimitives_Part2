e9d1dc7790b4bbb5ac0b495d9e20953ec4a40868
Chris Tralie <chris.tralie@gmail.com>
2019-09-02 21:11:14 -0400

    Mostly finished rayCircle GUI

---
+++ GeomPrimitives.js
@@ -0,0 +1,28 @@
+//Purpose: The engine behind the 3D primitive operations for Mini Assignment 1
+
+vec3 = glMatrix.vec3;
+
+//////////////////////////////////////////////
+///********    Geometry Functions   *******///
+//////////////////////////////////////////////
+//This is where you will have to fill in some code
+
+
+/**
+ * Find the intersection of the ray p0 + tv, t >= 0, with the
+ * circle centered at c with radius r.
+ * 
+ * @param {vec3} p0 Endpoint of the ray
+ * @param {vec3} v Direction of the ray
+ * @param {vec3} c Center of the circle
+ * @param {number} r Radius of the circle
+ * 
+ * @return {list of vec3} A list of intersection points, in the order in which
+ * the ray hits them.  If the ray doesn't hit any points, this list should be empty.
+ * Note that a ray can hit at most 2 points on a circle.
+ */
+function rayIntersectCircle(p0, v, c, r) {
+    //TODO: Fill this in
+    return []; //This is a dummy return
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Mini1_GeometricPrimitives_Part2
+# <a href = "http://www.ctralie.com/Teaching/CS476_F2019/">Ursinus CS 476, Fall 2019</a>
+## Instructor: Christopher Tralie
+## Mini Assignment 1 Part 2
+
+Please visit <a href = "http://www.ctralie.com/Teaching/CS476_F2019/Assignments/Mini1_GeometricPrimitives/#part2">http://www.ctralie.com/Teaching/CS476_F2019/Assignments/Mini1_GeometricPrimitives/#part2</a> for more information
