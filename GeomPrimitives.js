1f60fac3a7234da079d62aa9c650bfead0028e05
Chris Tralie <chris.tralie@gmail.com>
2019-09-04 09:33:26 -0400

    Finishing raySphere and barycentric APIs and GUI testers

--- GeomPrimitives.js
+++ GeomPrimitives.js
@@ -10,22 +10,39 @@ vec3 = glMatrix.vec3;
 
 /**
  * Find the intersection of the ray p0 + tv, t >= 0, with the
- * circle centered at c with radius r.
+ * sphere centered at c with radius r.
  * 
  * @param {vec3} p0 Endpoint of the ray
  * @param {vec3} v Direction of the ray
- * @param {vec3} c Center of the circle
- * @param {number} r Radius of the circle
+ * @param {vec3} c Center of the sphere
+ * @param {number} r Radius of the sphere
  * 
  * @return {list of vec3} A list of intersection points, in the order in which
  * the ray hits them.  If the ray doesn't hit any points, this list should be empty.
- * Note that a ray can hit at most 2 points on a circle.
+ * Note that a ray can hit at most 2 points on a sphere.
  */
-function rayIntersectCircle(p0, v, c, r) {
+function rayIntersectSphere(p0, v, c, r) {
     //TODO: Fill this in
-    return []; //This is a dummy return
+    return []; //This is a dummy value
 }
 
+
+/**
+ * 
+ * @param {vec3} a Point a on the triangle
+ * @param {vec3} b Point b on the triangle
+ * @param {vec3} c Point c on the triangle
+ * @param {vec3} p The point whose barycentric coordinates we seek
+ * 
+ * @return {vec3} An vec3 with the barycentric coordinates
+ * 				  [ba, bb, bc] such that ba + bb + bc = 1 and
+ * 				  ba, bb, bc > 0
+ * 				  If p is not inside of /\abc, then return [0, 0, 0]
+ */
+function getBarycentricCoords(a, b, c, p) {
+	//TODO: Fill this in
+	return vec3.create(); // TODO: This is a dummy value
+}
 
 
 
